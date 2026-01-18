93025ddfb6100fe2323760a8a15508cb2f69ceaa
Nishant Guvvada <nishant.guvvada@gmail.com>
2026-01-18 11:15:15 +0530

    mongo

--- mongo/index.js
+++ mongo/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://nishantguvvada:lMjNtGBsDaHyzsHk@cluster0.gon8jn3.mongodb.net/practice-mongo');
+mongoose.connect('');
 
 const User = mongoose.model('User', {
     username: { type: String, required: true },
