e5fbb1ddf1d0665eadf2653ce0d88586f2db4dc3
Nishant Guvvada <nishant.guvvada@gmail.com>
2026-01-18 11:14:47 +0530

    mongo

---
+++ mongo/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://nishantguvvada:lMjNtGBsDaHyzsHk@cluster0.gon8jn3.mongodb.net/practice-mongo');
+
+const User = mongoose.model('User', {
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ "message": "working" })
+});
+
+app.post('/signup', async (req, res) => {
+
+    try {
+
+        const user = await User.create({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        return res.status(200).json({ "message": `User created with ID: ${user._id}`});
+
+    } catch(err) {
+
+        return res.status(400).json({ "error": err });
+
+    }
+});
+
+app.listen(3000);
