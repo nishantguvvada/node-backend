a192acca80facfebce20400bfd22dbbcb3a5d326
Nishant Guvvada <nishant.guvvada@gmail.com>
2026-02-06 16:10:57 +0530

    updated

--- mongo/index.js
+++ mongo/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    return res.status(200).json({ "message": "working" })
+    return res.status(200).json({ "message": "working!" })
 });
 
 app.post('/signup', async (req, res) => {
